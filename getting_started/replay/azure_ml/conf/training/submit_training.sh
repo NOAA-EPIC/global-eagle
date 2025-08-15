bfa6edc91074f1a4825f341aeea5b7a8371b1ac0
Mariah Pope <mariah.pope@tomorrow.io>
2025-08-15 10:03:21 -0400

    add inference

--- getting_started/replay/azure_ml/conf/training/submit_training.sh
+++ getting_started/replay/azure_ml/conf/training/submit_training.sh
@@ -5,11 +5,11 @@ echo "Input data: $DATA_PATH"
 echo "Directory for output files: $OUTPUT_DIR"
 
 mkdir -p /workdir/data
-mkdir -p /workdir/model_output
+mkdir -p /workdir/training_output
 
 # Symlinks so we can connect with data within blob storage
 ln -s "$DATA_PATH" /workdir/data/replay.zarr
-ln -s "$OUTPUT_DIR" /workdir/model_output/
+ln -s "$OUTPUT_DIR" /workdir/training_output/
 
 export WORLD_SIZE=1
 export ANEMOI_BASE_SEED=1000
@@ -19,7 +19,7 @@ export RANK=0
 
 anemoi-training train --config-name=config.yaml
 
-cp -R /workdir/model_output/* $OUTPUT_DIR
+cp -R /workdir/training_output/* $OUTPUT_DIR
 echo "Model output saved to: $OUTPUT_DIR"
 
 # TODO: This should formally log within azureml somehow. 
