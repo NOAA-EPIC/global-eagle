2fcfe59cf64bf2daa42c83077363467b58f8e18f
Kris Booker <kristopher.booker@tomorrow.io>
2025-10-13 20:09:36 +0000

    Updating with local changes

--- nested_eagle/scientific_workflow/data/submit_hrrr.sh
+++ nested_eagle/scientific_workflow/data/submit_hrrr.sh
@@ -11,8 +11,10 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 
-source /ENTERPATHTOYOURMINICONDAINSCRATCH/miniconda3/bin/activate
+source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
 conda activate eagle
 module load openmpi gcc
+
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 srun ufs2arco hrrr.yaml --overwrite
