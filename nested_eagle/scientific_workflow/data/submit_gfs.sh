d6b6affee87dc2e642ca0ac3facf0672f7884cfa
Kris Booker <kristopher.booker@tomorrow.io>
2025-10-22 17:22:04 +0000

    Updating to use a different partition.

--- nested_eagle/scientific_workflow/data/submit_gfs.sh
+++ nested_eagle/scientific_workflow/data/submit_gfs.sh
@@ -4,11 +4,9 @@
 #SBATCH -o slurm/gfs_preprocessing.out
 #SBATCH -e slurm/gfs_preprocessing.err
 #SBATCH --account=epic
-#SBATCH --partition=u1-service
+#SBATCH --partition=u1-compute
 #SBATCH --mem=128g
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 
 source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
@@ -19,4 +17,4 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 python create_grids.py
 
-srun ufs2arco gfs.yaml --overwrite
+ufs2arco gfs.yaml --overwrite
--- nested_eagle/scientific_workflow/data/submit_hrrr.sh
+++ nested_eagle/scientific_workflow/data/submit_hrrr.sh
@@ -4,11 +4,8 @@
 #SBATCH -o slurm/hrrr_preprocessing.out
 #SBATCH -e slurm/hrrr_preprocessing.err
 #SBATCH --account=epic
-#SBATCH --partition=u1-service
+#SBATCH --partition=u1-compute
 #SBATCH --mem=128g
-#SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 
 source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
