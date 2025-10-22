4c851d107a6b7f574f990c888d4b97c9b19e0206
Kris Booker <kristopher.booker@tomorrow.io>
2025-10-22 20:58:42 +0000

    Reverting my partition changes, there's a reason for this.

--- nested_eagle/scientific_workflow/data/submit_gfs.sh
+++ nested_eagle/scientific_workflow/data/submit_gfs.sh
@@ -4,16 +4,17 @@
 #SBATCH -o slurm/gfs_preprocessing.out
 #SBATCH -e slurm/gfs_preprocessing.err
 #SBATCH --account=epic
-#SBATCH --partition=u1-compute
+#SBATCH --partition=u1-service
 #SBATCH --mem=128g
 #SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
 
 source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
 conda activate eagle
 module load openmpi gcc
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
-python create_grids.py
 
 ufs2arco gfs.yaml --overwrite
--- nested_eagle/scientific_workflow/data/submit_hrrr.sh
+++ nested_eagle/scientific_workflow/data/submit_hrrr.sh
@@ -4,8 +4,13 @@
 #SBATCH -o slurm/hrrr_preprocessing.out
 #SBATCH -e slurm/hrrr_preprocessing.err
 #SBATCH --account=epic
-#SBATCH --partition=u1-compute
+#SBATCH --partition=u1-service
 #SBATCH --mem=128g
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+
 
 source /scratch4/NAGAPE/epic/role-epic/miniconda/bin/activate
 conda activate eagle
@@ -13,4 +18,4 @@ module load openmpi gcc
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-srun ufs2arco hrrr.yaml --overwrite
+ufs2arco hrrr.yaml --overwrite
